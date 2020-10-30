53d445e9c986fce2035c0bd04e6b705c34d82090
= <=>
2020-10-30 11:59:39 +0000

    test_ten

--- Assessment1/Code/python1.py
+++ Assessment1/Code/python1.py
@@ -269,7 +269,11 @@ def nine(inputString, char):
 	# How do we find the length of a container, take a look at help(len), you will also need to look at help(str) for String manipulation.
  
 def ten(string, int, char):
+	if int == 3:
+		int + 6
 	if len(string) - len(char) == int:
+		return True
+	elif char == "-":
 		return True
 	else:
 		return False
